09198e4c959512dd8c9d33d55572cea13ff5f119
s1120417-collab <s1120417@dmhs.kh.edu.tw>
2025-10-16 14:51:36 +0800

    hw5

--- test1/main.cpp
+++ test1/main.cpp
@@ -2,9 +2,28 @@
 using namespace std;
 
 int main() {
-    int sum = 0;
-    for (int i = 1; i <= 50; i++) {
-        if ((i % 7) == 0) cout << i << " ";
+    // 第一部分：印出直角三角形的星星圖案
+    for (int i = 1; i <= 6; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
     }
+
+    cout << endl;  // 分隔一下
+
+    // 第二部分：印出九九乘法表
+    int i = 1;
+    int j;
+    while (i <= 9) {
+        j = 1;
+        while (j <= 9) {
+            cout << i << " * " << j << " = " << i * j << "\t";
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+
     return 0;
 }

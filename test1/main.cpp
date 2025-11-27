64eed657cbac56c30b03755e8f501bcc1eeb6946
s1120417-collab <s1120417@dmhs.kh.edu.tw>
2025-11-27 14:10:51 +0800

    hw10

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
+#include <cstdlib>   // rand(), srand()
+#include <ctime>     // time()
+
 using namespace std;
 
-int accumulate(int n);
-
 int main() {
-    int number = 100;
-    cout << "從1加到" << number << "總合為: " << accumulate(number);
-    return 0;
-}
+    const int N = 10;    // 陣列大小
+    int arr[N];
 
-int accumulate(int n) {
-    if (n == 1) {
-        return 1;
+    // ---- 亂數種子 ----
+    srand(time(NULL));
+
+    // ---- 產生 0~99 的亂數陣列 ----
+    cout << "原始亂數陣列: ";
+    for (int i = 0; i < N; i++) {
+        arr[i] = rand() % 100;
+        cout << arr[i] << " ";
     }
-    return n + accumulate(n - 1);
+    cout << endl;
+
+    // ---- 選擇排序 (Selection Sort) ----
+    for (int i = 0; i < N - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < N; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // 交換
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    // ---- 排序後輸出 ----
+    cout << "排序後陣列: ";
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
